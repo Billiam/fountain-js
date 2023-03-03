828c9d2d6ad939d2afdfcf206d61509fe3596a99
Billiam <billiamthesecond@gmail.com>
2023-03-03 11:11:25 -0600

    Remove unnecessary non-capturing groups

--- src/regex.ts
+++ src/regex.ts
@@ -1,24 +1,24 @@
 export const regex = {
     title_page: /^([^\n]+:)/gim,
 
-    scene_heading: /^((?:\*{0,3}_?)?(?:(?:int|i)\.?\/(?:ext|e)|int|ext|est)[. ].*)|^(?:\.(?!\.+))(.+)/i,
+    scene_heading: /^((?:\*{0,3}_?)?(?:(?:int|i)\.?\/(?:ext|e)|int|ext|est)[. ].*)|^\.(?!\.+)(.+)/i,
     scene_number: /( *#(.+)# *)/,
 
-    transition: /^((?:FADE (?:TO BLACK|OUT)|CUT TO BLACK)\.|.+ TO:)|^(?:> *)(.+)/,
+    transition: /^((?:FADE (?:TO BLACK|OUT)|CUT TO BLACK)\.|.+ TO:)|^> *(.+)/,
 
-    dialogue: /^(?:([A-Z*_][0-9A-Z ._\-']*(?:\(.*\))? *)|@([A-Za-z*_][0-9A-Za-z (._\-')]*))(\^?)?(?:\n(?!\n+))([\s\S]+)/,
+    dialogue: /^(?:([A-Z*_][0-9A-Z ._\-']*(?:\(.*\))? *)|@([A-Za-z*_][0-9A-Za-z (._\-')]*))(\^?)?\n(?!\n+)([\s\S]+)/,
     parenthetical: /^(\(.+\))$/,
 
     action: /^(.+)/g,
-    centered: /^(?:> *)(.+)(?: *<)(\n.+)*/g,
+    centered: /^> *(.+) *<(\n.+)*/g,
 
     lyrics: /^~(?! ).+(?:\n.+)*/,
 
-    section: /^(#+)(?: *)(.*)/,
-    synopsis: /^(?:=(?!=+) *)(.*)/,
+    section: /^(#+) *(.*)/,
+    synopsis: /^:=(?!=+) *(.*)/,
 
-    note: /^(?:\[{2}(?!\[+))(.+)(?:]{2}(?!\[+))$/,
-    note_inline: /(?:\[{2}(?!\[+))([\s\S]+?)(?:]{2}(?!\[+))/g,
+    note: /^\[{2}(?!\[+)(.+)]{2}(?!\[+)$/,
+    note_inline: /\[{2}(?!\[+)([\s\S]+?)]{2}(?!\[+)/g,
     boneyard: /(^\/\*|^\*\/)$/g,
 
     page_break: /^={3,}$/,
